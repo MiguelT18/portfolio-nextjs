59b30bda2685cc5ea39b1bc25b6ad26a427d39f1
miguel <miguel.teranj02@gmail.com>
2024-07-09 17:58:56 -0400

    create a page for the content for every course in courses data from json file

--- src/app/courses/[category]/[courseId]/content/page.tsx
+++ src/app/courses/[category]/[courseId]/content/page.tsx
@@ -1,9 +1,5 @@
 import React from 'react'
 
-export default function ContentCourse() {
-  return (
-    <main>
-      <h1>Contenido del curso</h1>
-    </main>
-  )
+export default function ContentPage() {
+  return <main>ContentPage</main>
 }
