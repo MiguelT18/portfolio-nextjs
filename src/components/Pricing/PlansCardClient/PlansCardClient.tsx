d614de71d024f1db66a4a09c49cc4843cccc59ea
miguel <miguel.teranj02@gmail.com>
2024-07-16 21:00:29 -0400

    Testing paypal payment methods consuming an API

--- src/components/Pricing/PlansCardClient/PlansCardClient.tsx
+++ src/components/Pricing/PlansCardClient/PlansCardClient.tsx
@@ -1,29 +1,51 @@
 'use client'
 
-import PrimaryButton from '@/components/UI/Buttons/PrimaryButton'
 import styles from './styles.module.css'
+import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js'
 
 export default function PlansCardClient({ plans }: any) {
-  const handlePay = async (plan: any) => {
-    const response = await fetch('/api/checkout', {
-      method: 'POST'
-    })
-    const data = await response.json()
-
-    console.log(data)
-  }
-
   return (
     <ul className={styles.plansContainer}>
       {plans.map((plan: any) => (
-        <li key={plan.id}>
-          <div className={styles.planCardContainer}>
-            <h2>{plan.name}</h2>
-            <span>{plan.price}</span>
-            <PrimaryButton onClick={() => handlePay(plan)}>Pagar</PrimaryButton>
-          </div>
+        <li key={plan.id} className={styles.planCardContainer}>
+          <h2>{plan.name}</h2>
+          <span>{plan.price} USD</span>
+          <PayPalScriptProvider
+            options={{
+              clientId:
+                'AVChxnuzIw1Et90hOzl29JiwOUnPL1hBDZBXOKbVgBK3hC-QwdLIWy0_c3DcncK3a6esv0sTvRW9GeWT'
+            }}
+          >
+            <PayPalButtons
+              style={{
+                color: 'silver',
+                layout: 'vertical',
+                label: 'pay',
+                shape: 'pill'
+              }}
+              createOrder={async () => {
+                try {
+                  const res = await fetch('/api/checkout', {
+                    method: 'POST'
+                  })
+                  const order = await res.json()
+                  console.log(order)
+                  return order.id
+                } catch (error) {
+                  console.error(error)
+                  alert('Error creating order')
+                }
+              }}
+            />
+          </PayPalScriptProvider>
         </li>
       ))}
     </ul>
   )
 }
+
+/*
+createOrder={() => {}}
+onCancel={() => {}}
+onApprove={() => {}}
+*/
